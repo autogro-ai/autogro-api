5e43f8b9c8dff291fd711792d28a81a8feb2096a
Matt Chandler <matt.chandleraz@gmail.com>
2023-12-03 09:01:00 -0600

    Users CRUD working

--- resources/users.py
+++ resources/users.py
@@ -2,16 +2,35 @@ from flask import Blueprint, g, jsonify, request
 
 users_api = Blueprint('users', __name__) 
 
+# List
+@users_api.route('/users', methods=['GET'])
+def list_users():
+    db = g.db
+    cursor = g.db.cursor()
+    cursor.execute('SELECT userID, FirstName, LastName, EmailAddress, active FROM users')
+    data = cursor.fetchall() 
+    return jsonify(data)
+
 # Create
 @users_api.route('/users', methods=['POST']) 
 def create_user():
     db = g.db
     cursor = g.db.cursor()
 
-    cursor.execute('INSERT INTO users (name, email) VALUES (?, ?)', 
-              (request.json['name'], request.json['email']))
+    userData = request.get_json();
 
-    return jsonify({'message': 'User created successfully'}), 201
+    firstName = userData['firstName']
+    lastName = userData['lastName']  
+    emailAddress = userData['emailAddress']
+
+
+    cursor.execute('INSERT INTO users (FirstName, LastName, EmailAddress) VALUES (%s, %s, %s)', (firstName, lastName, emailAddress))
+    
+    userID = cursor.lastrowid
+
+    db.commit()
+
+    return jsonify({'userID': userID, 'message': 'User created successfully'}), 201
 
 # Read
 @users_api.route('/users/<int:id>')
@@ -28,16 +47,69 @@ def update_user(id):
     db = g.db
     cursor = g.db.cursor()    
     
-    cursor.execute('UPDATE users SET name = ?, email = ? WHERE id = ?', 
-              (request.json['name'], request.json['email'], id))
-    
-    return jsonify({'message': 'User updated successfully'})
+    query = """
+            UPDATE users  
+            SET
+            FirstName = IF( %(new_FirstName)s <> FirstName, %(new_FirstName)s, FirstName), 
+            LastName = IF( %(new_LastName)s <> LastName, %(new_LastName)s, LastName), 
+            EmailAddress = IF( %(new_EmailAddress)s <> EmailAddress, %(new_EmailAddress)s, EmailAddress) 
+            WHERE userID = %(userID)s
+            """
 
-# Delete
+    data = {  
+        "new_FirstName": request.json['firstName'],
+        "new_LastName": request.json['lastName'],
+        "new_EmailAddress": request.json['emailAddress'],
+        "userID": id
+    }
+    
+
+    cursor.execute(query, data)    
+
+    db.commit()
+
+    msg = 'User updated successfully'
+
+    return jsonify({'message': msg})
+
+# DEACTIVATE by way of DELETE
 @users_api.route('/users/<int:id>', methods=['DELETE'])  
 def delete_user(id):
     db = g.db
     cursor = g.db.cursor()    
 
-    cursor.execute('DELETE FROM users WHERE id = ?', (id,))
-    return jsonify({'message': 'User deleted successfully'})
+    query = "UPDATE users SET active = %s WHERE userID = %s"
+    active_value = 0
+
+    cursor.execute(query, (active_value, id))  
+    db.commit()  
+
+    return jsonify({'message': 'User deactivated'})
+
+# DEACTIVATE
+@users_api.route('/users/<int:id>/Deactivate', methods=['PUT'])  
+def deactivate_user(id):
+    db = g.db
+    cursor = g.db.cursor()    
+
+    query = "UPDATE users SET active = %s WHERE userID = %s"
+    active_value = 0
+
+    cursor.execute(query, (active_value, id))  
+    db.commit()  
+
+    return jsonify({'message': 'User Deactivated'})
+
+# ACTIVATE
+@users_api.route('/users/<int:id>/Activate', methods=['PUT'])  
+def activate_user(id):
+    db = g.db
+    cursor = g.db.cursor()    
+
+    query = "UPDATE users SET active = %s WHERE userID = %s"
+    active_value = 1
+
+    cursor.execute(query, (active_value, id))  
+    db.commit()  
+
+    return jsonify({'message': 'User Activated'})
