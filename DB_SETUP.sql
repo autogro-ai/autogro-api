bca7ee89c4a8a72103be9b480b6e6747e275da00
Matt Chandler <matt.chandleraz@gmail.com>
2023-12-03 08:59:57 -0600

    lastest schema

--- DB_SETUP.sql
+++ DB_SETUP.sql
@@ -3,14 +3,13 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 02, 2023 at 06:06 PM
+-- Generation Time: Dec 03, 2023 at 02:59 PM
 -- Server version: 5.7.39
 -- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 --
 -- Database: `autogro`
@@ -64,6 +63,7 @@ CREATE TABLE `gro_models` (
 
 CREATE TABLE `users` (
   `userID` int(11) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '0',
   `FirstName` varchar(100) DEFAULT NULL,
   `LastName` varchar(100) DEFAULT NULL,
   `EmailAddress` varchar(120) DEFAULT NULL,
